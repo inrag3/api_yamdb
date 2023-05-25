16efba7747b11eafa2b46c128f42dca3bc3f5cb0
Кирилл Антонов <antonov.kirill1991@yandex.ru>
2023-05-25 18:07:51 +0300

    Добавил админ панели

--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -1,5 +1,4 @@
 from django.shortcuts import get_object_or_404
-from rest_framework.pagination import LimitOffsetPagination
 from rest_framework import (
     permissions,
     viewsets,
@@ -14,7 +13,6 @@ from reviews.models import Review, Title
 class ReviewViewSet(viewsets.ModelViewSet):
     serializer_class = ReviewSerializer
     permission_classes = (permissions.IsAuthenticatedOrReadOnly,)
-    pagination_class = LimitOffsetPagination
 
     def get_queryset(self):
         title = get_object_or_404(Title, id=self.kwargs.get('title_id'))
@@ -28,7 +26,6 @@ class ReviewViewSet(viewsets.ModelViewSet):
 class CommentViewSet(viewsets.ModelViewSet):
     serializer_class = CommentSerializer
     permission_classes = (permissions.IsAuthenticatedOrReadOnly,)
-    pagination_class = LimitOffsetPagination
 
     def perform_create(self, serializer):
         review = get_object_or_404(
--- api_yamdb/reviews/admin.py
+++ api_yamdb/reviews/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
 
-# Register your models here.
+from reviews.models import Comment, Review
+
+
+class ReviewAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'author', 'score', 'pub_date')
+    search_fields = ('title', 'author', 'text')
+    list_filter = ('pub_date', 'score', 'author')
+    empty_value_display = '-пусто-'
+
+
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('id', 'text', 'author', 'pub_date')
+    search_fields = ('text', 'author')
+    list_filter = ('author', 'pub_date')
+    empty_value_display = '-пусто-'
+
+
+admin.site.register(Review, ReviewAdmin)
+admin.site.register(Comment, CommentAdmin)
