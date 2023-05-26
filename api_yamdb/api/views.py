a68a576770a2b73341e6d5891146d5e242ca3760
Екатерина Густова <antinpanic@gmail.com>
2023-05-26 11:40:52 +0300

    create mixin.py

--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -1,4 +1,3 @@
-from rest_framework.response import Response
 from rest_framework import viewsets, mixins, filters
 from django_filters.rest_framework import DjangoFilterBackend
 
@@ -7,34 +6,7 @@ from .serializers import (TitleSerializer,
                           TitleSlugSerializer,
                           GenreSerializer,
                           CategorySerializer)
-
-
-class TitleCreateMixin(mixins.CreateModelMixin,
-                       mixins.UpdateModelMixin):
-    def change_data(self, response):
-        category_object = Category.objects.get(slug=response.data['category'])
-        serialised_category = CategorySerializer(category_object)
-        response.data['category'] = serialised_category.data
-        genres = response.data['genre']
-        response.data['genre'] = []
-        for slug in genres:
-            genre_object = Genre.objects.get(slug=slug)
-            serialised_genre = GenreSerializer(genre_object)
-            response.data['genre'].append(serialised_genre.data)
-        return response
-
-    def update(self, request, *args, **kwargs):
-        partial = kwargs.get('partial', None)
-        if partial is None:
-            return Response({
-                            "detail": "Method \"PUT\" not allowed."
-                            }, '405')
-        response = super().update(request, *args, **kwargs)
-        return self.change_data(response)
-
-    def create(self, request, *args, **kwargs):
-        response = super().create(request, *args, **kwargs)
-        return self.change_data(response)
+from .mixins import CreateListRetrieveViewSet, TitleCreateMixin
 
 
 class TitleViewSet(TitleCreateMixin,
@@ -52,10 +24,7 @@ class TitleViewSet(TitleCreateMixin,
         return TitleSerializer
 
 
-class GenreViewSet(mixins.CreateModelMixin,
-                   mixins.ListModelMixin,
-                   mixins.DestroyModelMixin,
-                   viewsets.GenericViewSet):
+class GenreViewSet(CreateListRetrieveViewSet):
     queryset = Genre.objects.all()
     serializer_class = GenreSerializer
     filter_backends = (filters.SearchFilter,)
@@ -63,10 +32,7 @@ class GenreViewSet(mixins.CreateModelMixin,
     lookup_field = 'slug'
 
 
-class CategoryViewSet(mixins.CreateModelMixin,
-                      mixins.ListModelMixin,
-                      mixins.DestroyModelMixin,
-                      viewsets.GenericViewSet):
+class CategoryViewSet(CreateListRetrieveViewSet):
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
     filter_backends = (filters.SearchFilter,)
