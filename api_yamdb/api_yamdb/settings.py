5b9ae23453925ef091ad23662d6611dc0c7230f2
inrage <70843287+inrag3@users.noreply.github.com>
2023-05-26 19:00:00 +0200

    Add User model

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -21,6 +21,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'users',
 ]
 
 MIDDLEWARE = [
@@ -101,3 +102,13 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = ((BASE_DIR / 'static/'),)
+
+
+USER = 'user'
+MODERATOR = 'moderator'
+ADMIN = 'admin'
+ROLES = (
+    (USER, 'user'),
+    (MODERATOR, 'moderator'),
+    (ADMIN, 'admin'),
+)
---
+++ api_yamdb/users/models.py
@@ -0,0 +1,46 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+from api_yamdb.settings import ROLES, USER, ADMIN, MODERATOR
+from django.core.validators import RegexValidator
+
+class User(AbstractUser):
+    username = models.CharField(
+        max_length=100,
+        unique=True,
+        validators=[
+            RegexValidator(
+                regex=r'^[\w.@+-]+$'
+            )
+        ]
+    )
+    email = models.EmailField(
+        max_length=254,
+        unique=True,
+    )
+    first_name = models.CharField(
+        max_length=150,
+        blank=True
+    )
+    last_name = models.CharField(
+        max_length=150,
+        blank=True
+    )
+    bio = models.TextField(
+        blank=True,
+    )
+    role = models.CharField(
+        choices=ROLES,
+        default=USER
+    )
+
+    def is_user(self):
+        return self.role == USER
+
+    def is_moderator(self):
+        return self.role == MODERATOR
+
+    def is_admin(self):
+        return self.role == ADMIN or self.is_staff
+
+    def __str__(self) -> str:
+        return f'{self.username}'
