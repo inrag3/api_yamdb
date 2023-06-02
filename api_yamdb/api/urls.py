38abe85fcff7764297c933216abaa3eae8e0dd24
inrage <70843287+inrag3@users.noreply.github.com>
2023-06-02 12:10:10 +0300

    Fix user sturcture and getting code

--- api_yamdb/api/urls.py
+++ api_yamdb/api/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from users.views import (
+from .views import (
     UserViewSet,
     TokenReceiveViewSet,
     UserRegistrationViewSet,
--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -1,8 +1,17 @@
-from rest_framework import viewsets, mixins, filters
+from rest_framework import viewsets, mixins, filters, permissions, status
+from django.contrib.auth import get_user_model
+from rest_framework.permissions import IsAuthenticated
+from rest_framework_simplejwt.tokens import AccessToken
+from rest_framework.response import Response
 from django_filters.rest_framework import DjangoFilterBackend
+from django.contrib.auth.tokens import default_token_generator
 from django.shortcuts import get_object_or_404
+from django.core.mail import send_mail
+from rest_framework.decorators import action
+from api_yamdb.settings import EMAIL
 from reviews.models import Title, Genre, Category, Review, Title
-from users.permissions import (
+from .permissions import (
+    IsAdmin,
     IsAdminOrModeratorOrOwnerOrReadOnly,
     IsAdminOrReadOnly,
 )
@@ -13,8 +22,13 @@ from .serializers import (
     CategorySerializer,
     CommentSerializer,
     ReviewSerializer,
+    UserRegistrationSerializer,
+    UserSerializer,
+    TokenReceiveSerializer,
 )
 from .mixins import CreateListRetrieveViewSet, TitleCreateMixin
+
+User = get_user_model()
 
 
 class TitleViewSet(TitleCreateMixin,
@@ -93,3 +107,75 @@ class CommentViewSet(viewsets.ModelViewSet):
             title__id=self.kwargs.get('title_id'),
         )
         return review.comments.all()
+
+
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    permission_classes = (IsAuthenticated, IsAdmin, )
+    lookup_field = 'username'
+    filter_backends = (filters.SearchFilter, )
+    search_fields = ('username', )
+    http_method_names = ('get', 'post', 'patch', 'delete', )
+
+    @action(
+        methods=['get', 'patch'],
+        detail=False,
+        permission_classes=(IsAuthenticated, )
+    )
+    def me(self, request):
+        serializer = UserSerializer(request.user)
+        if request.method == 'PATCH':
+            data = request.data.dict()
+            if 'role' in data:
+                data.pop('role')
+            serializer = UserSerializer(
+                request.user,
+                data=data,
+                partial=True,
+            )
+            serializer.is_valid(raise_exception=True)
+            serializer.save()
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+
+class TokenReceiveViewSet(mixins.CreateModelMixin,
+                          viewsets.GenericViewSet):
+    queryset = User.objects.all()
+    permission_classes = (permissions.AllowAny, )
+    serializer_class = TokenReceiveSerializer
+
+    def create(self, request):
+        serializer = TokenReceiveSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        username = serializer.validated_data['username']
+        confirmation_code = serializer.validated_data['confirmation_code']
+        user = get_object_or_404(User, username=username)
+        if default_token_generator.check_token(user, confirmation_code):
+            data = {'token': str(AccessToken.for_user(user))}
+            return Response(data, status=status.HTTP_200_OK)
+        return Response(
+            "wrong confirmation code",
+            status=status.HTTP_400_BAD_REQUEST
+        )
+
+
+class UserRegistrationViewSet(mixins.CreateModelMixin,
+                              viewsets.GenericViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserRegistrationSerializer
+    permission_classes = (permissions.AllowAny,)
+
+    def create(self, request):
+        serializer = UserRegistrationSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        user, _ = User.objects.get_or_create(**serializer.validated_data)
+        confirmation_code = default_token_generator.make_token(user)
+        print(confirmation_code)
+        send_mail(
+            subject='Код подтверждения.',
+            message=f'Код подтверждения: {confirmation_code}',
+            from_email=EMAIL,
+            recipient_list=(user.email,),
+        )
+        return Response(serializer.data, status=status.HTTP_200_OK)
